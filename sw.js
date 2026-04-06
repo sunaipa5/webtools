341a1db8d5af1124b74a5d2fe67098f0e1458a6f
sunaipa <67613319+sunaipa5@users.noreply.github.com>
2026-04-06 19:02:08 +0300

    Fix service worker offline cache for routes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tool-cache-v1";
+const CACHE_NAME = "v1";
 const ASSETS = [
   "/",
   "/index.html",
@@ -7,37 +7,47 @@ const ASSETS = [
   "/manifest.json",
   "/favicon.webp",
   "/icon-192.png",
+  //--- Routes ---
+  "/base64/",
   "/base64/index.html",
+  "/iconpress/",
   "/iconpress/index.html",
   "/iconpress/jszip.min.js",
+  "/sleep-calculator/",
   "/sleep-calculator/index.html",
+  "/svg-opt/",
   "/svg-opt/index.html",
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      return Promise.allSettled(ASSETS.map((url) => cache.add(url)));
     }),
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key)),
+        keys.filter((key) => key !== CACHE_NAME).map((k) => caches.delete(k)),
       );
     }),
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        console.log("Network request failed and not in cache");
+      });
     }),
   );
 });
